0c12cd27d9b5492e0b3b6d1dd86ead1b85c0b8c1
Maansi Kalwar <maansikalwar@gmail.com>
2025-06-23 14:32:58 +0530

    Add files via upload

---
+++ Two Sum_Day 1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_map = {}  # This is like a notebook where we remember numbers we've seen
+
+        for i, num in enumerate(nums):  # Look at each number and its position
+            difference = target - num  # What number do we need to reach the target?
+ 
+            if difference in num_map:
+                return [num_map[difference], i]  # We found the pair!
+
+            num_map[num] = i  # Remember this number and where we saw it
