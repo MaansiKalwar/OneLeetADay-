be0d9a289f169bac65f8a194970290caf257c790
Maansi Kalwar <maansikalwar@gmail.com>
2025-06-25 22:40:45 +0530

    Two Sum_Day 1.py

--- Two Sum_Day 1.py
+++ Two Sum_Day 1.py
@@ -3,7 +3,7 @@ class Solution:
         num_map = {}  # This is like a notebook where we remember numbers we've seen
 
         for i, num in enumerate(nums):  # Look at each number and its position
-            difference = target - num  # What number do we need to reach the target?
+            difference = target - num  # What number do we need to reach the target? 
  
             if difference in num_map:
                 return [num_map[difference], i]  # We found the pair!  
